224c58a67d4db3ff0c2e8132c30338bc574703b6
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-10-11 09:18:01 -0400

    added comments and plot

--- ptb_analysis/analysis/compute_trajectory_and_info.py
+++ ptb_analysis/analysis/compute_trajectory_and_info.py
@@ -1,3 +1,7 @@
+"""
+This script extract the monkey trajectory and info related to each trial, and saves it into an array.
+
+"""
 import os
 import re
 from copy import deepcopy
@@ -6,6 +10,7 @@ import numpy as np
 
 from ptb_analysis.process import (create_session_trajectory_info,
                                   merge_trajectories)
+import matplotlib.pyplot as plt
 
 # path to the folder where all the math files are located
 path = "/Users/ebalzani/Desktop/firefly_data/"
@@ -66,6 +71,45 @@ for fh in os.listdir(path):
         is_rewarded_merged = deepcopy(is_rewarded)
 
     cc += 1
+
+# here:
+#  - traj_merged has shape (n_trials, n_samples, 2), two is for x,y coord.
+#    plt.plot(*traj_merged[100].T) will plot a trial
+#  - ptb_index_merged: shape (n_trials) contains the index of the sample
+#    corresponding to when the ptb was delivered.
+#  - session_list_merged: an array (num_trials,) with the session label
+#  - trial_ids_merged: (num_trials,) the id of the trial
+#  - target_pos_merged: (num_trials, 2) the target position for each trial
+#  - is_rewarded_merged: (num_trials,) bool if the trial was rewarded
+#  - ptb_velocity_merged: (n_trials, n_samples, 2) velocity vx and vy of the ptb
+#  - tot_velocity_merged: (n_trials, n_samples, 2) velocity vx and vy of the monkey (ptb + joystick displacement)
+
+# plot two trials, rewarded and unrewarded
+tr_rew = np.where(is_rewarded_merged)[0][0]
+tr_unrew = np.where(~is_rewarded_merged)[0][0]
+
+
+# compute rew zone (it shoould be 60cm, ask JP)
+ang = np.linspace(0, np.pi*2,200)
+rew_zone = 60 * np.c_[np.cos(ang), np.sin(ang)]
+
+fig, axs = plt.subplots(1,2)
+axs[0].plot(*traj_merged[tr_rew].T)
+axs[0].plot(*target_pos_merged[tr_rew].T, "or", markersize=10)
+axs[0].set_xlabel("cm")
+axs[0].set_ylabel("cm")
+axs[0].plot(*(rew_zone + target_pos_merged[tr_rew]).T, color="r")
+plt.legend(["trajectory", "target", "reward zone"])
+axs[0].set_aspect('equal')
+
+axs[1].plot(*traj_merged[tr_unrew].T)
+axs[1].plot(*target_pos_merged[tr_unrew].T, "or", markersize=10)
+axs[1].set_xlabel("cm")
+axs[1].set_ylabel("cm")
+axs[1].plot(*(rew_zone + target_pos_merged[tr_unrew]).T, color="r")
+plt.legend(["trajectory", "target", "reward zone"])
+axs[1].set_aspect('equal')
+plt.show()
 
 
 np.savez(
